30bbc545087e254f404c949890ef63b747e85e97
Yonghwi Kwon <yonghwi.kwon@hotmail.com>
2024-01-01 20:44:42 +0800

    up

--- class/softsec/pin/pin.sh
+++ class/softsec/pin/pin.sh
@@ -1,6 +1,17 @@
-mkdir pin
-cd pin
+
 wget https://yonghwi-kwon.github.io/class/softsec/pin/pin-3.28-98749-g6643ecee5-gcc-linux.tar.gz
 
 tar -xvf pin-3.28-98749-g6643ecee5-gcc-linux.tar.gz
 
+mv pin-3.28-98749-g6643ecee5-gcc-linux pin
+
+rm pin-3.28-98749-g6643ecee5-gcc-linux.tar.gz
+
+cd pin
+
+wget https://yonghwi-kwon.github.io/class/softsec/script/pin.env
+echo "cd $PWD" >> ~/.bashrc
+echo "source $PWD/pin.env" >> ~/.bashrc
+echo "cd" >> ~/.bashrc
+
+echo "Please type `source pin.env` on the terminal or restart the terminal window."
