dc0a2ee276bf6e4e69b949603360518b2434bf8b
Yonghwi Kwon <yonghwi.kwon@hotmail.com>
2024-01-01 20:26:30 +0800

    mb

--- class/softsec/project/prj1.sh
+++ class/softsec/project/prj1.sh
@@ -1,7 +1,7 @@
 mkdir prj1
 cd prj1
 wget https://yonghwi-kwon.github.io/class/softsec/project/moon-buggy.zip
-unzip moon-buggy-master.zip
+unzip moon-buggy.zip
 
 sudo apt-get install autoconf automake texinfo
 sudo apt-get install libncurses5-dev libncursesw5-dev
