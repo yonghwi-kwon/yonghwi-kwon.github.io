c505422f65253261e9a3ca9eab14b790e115ef9e
Yonghwi Kwon <yonghwi.kwon@hotmail.com>
2024-01-27 17:04:00 -0800

    aslr.sh

--- class/softsec/script/aslr/aslr.sh
+++ class/softsec/script/aslr/aslr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ -z $1 ]]; then
+if [[ -z $1 || "$1" == "test" ]]; then
 # TEST ASLR
 echo "#include <stdio.h>" > a.c
 echo "int main() { printf(\"%p\\n\", main); return 0; }" >> a.c
@@ -12,21 +12,24 @@ comp_value=$?
 
 if [ $comp_value -eq 1 ]
 then
-    echo "ASLR is on. Turning off would make your debugging easy. To turn off ASLR, run `./aslr.sh off`"
+    echo "ASLR is on. Turning off would make your debugging easy. To turn off ASLR, run './aslr.sh off'"
 else
-    echo "ASLR is off. Turning on would make your system secure. To turn on ASLR, run `./aslr.sh on`"
+    echo "ASLR is off. Turning on would make your system secure. To turn on ASLR, run './aslr.sh on'"
 fi
 
 rm r1
 rm r2
 rm a.out
 rm a.c
+exit;
 fi
 
 if [ "$1" == "on" ]; then
 echo 2 | sudo tee /proc/sys/kernel/randomize_va_space
+exit;
 fi
 
 if [ "$1" == "off" ]; then
 echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
+exit;
 fi
